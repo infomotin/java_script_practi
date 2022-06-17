7787e822a1a009ce551487fb7477cd3c173b16a4
Abdul Motin <motin.mmk.jr@gmail.com>
2022-06-17 11:00:13 +0600

    Add again to +

--- js/object_js/function_test.js
+++ js/object_js/function_test.js
@@ -255,4 +255,70 @@ function square (x) {
     return x * x;
 }
 
-let s = sq
+let s = sq(4)
+console.log(s);
+ 
+let o = {square: function(x) { return x*x; }}; // An object literal
+let y = o.square(16);
+
+let a = [x => x*x, 20]; // An array literal
+a[0](a[1]) // => 400
+
+// We define some simple functions here
+function add(x,y) { return x + y; }
+function subtract(x,y) { return x - y; }
+function multiply(x,y) { return x * y; }
+function divide(x,y) { return x / y; }
+
+
+// Here's a function that takes one of the preceding functions
+// as an argument and invokes it on two operands
+function operate(operator, operand1, operand2) {
+ return operator(operand1, operand2);
+}
+
+// We could invoke this function like this to compute the value (2+3) + (4*5):
+let i = operate(add, operate(add, 2, 3), operate(multiply, 4, 5));
+// For the sake of the example, we implement the simple functions again,
+// this time within an object literal;
+const operators = {
+ add: (x,y) => x+y,
+ subtract: (x,y) => x-y,
+ multiply: (x,y) => x*y,
+ divide: (x,y) => x/y,
+ pow: Math.pow // This works for predefined functions too
+};
+
+// This function takes the name of an operator, looks up that operator
+// in the object, and then invokes it on the supplied operands. Note
+// the syntax used to invoke the operator function.
+function operate2(operation, operand1, operand2) {
+ if (typeof operators[operation] === "function") {
+ return operators[operation](operand1, operand2);
+ }
+ else throw "unknown operator";
+}
+operate2("add", "hello", operate2("add", " ", "world")) // => "hello world"
+operate2("pow", 10, 2) // => 100
+// Initialize the counter property of the function object.
+// Function declarations are hoisted so we really can
+// do this assignment before the function declaration.
+uniqueInteger.counter = 0;
+// This function returns a different integer each time it is called.
+// It uses a property of itself to remember the next value to be returned.
+function uniqueInteger() {
+    return uniqueInteger.counter++; // Return and increment counter property
+    }
+uniqueInteger() // => 0
+uniqueInteger() // => 1
+// Compute factorials and cache results as properties of the function itself.
+function factorial(n) {
+ if (Number.isInteger(n) && n > 0) { // Positive integers only
+ if (!(n in factorial)) { // If no cached result
+ factorial[n] = n * factorial(n-1); // Compute and cache it
+ }
+ return factorial[n]; // Return the cached result
+ } else {
+ return NaN; // If input was bad
+ }
+}
